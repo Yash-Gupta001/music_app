d67716da1bc3632d2b02ef7425616267a395e4e9
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-16 23:57:09 +0530

    changed app icon

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:music_app/common/widget/button/basic_button.dart';
+import 'package:music_app/core/configs/assets/app_images.dart';
 import 'package:music_app/core/configs/assets/app_vectors.dart';
+import 'package:music_app/core/configs/theme/app_color.dart';
 
 class SignupOrSignin extends StatelessWidget {
   const SignupOrSignin({super.key});
@@ -23,8 +26,79 @@ class SignupOrSignin extends StatelessWidget {
               AppVectors.bottomPattern
             ),
           ),
+
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(
+              AppImages.authBG
+            )
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AppVectors.logo
+                  ),
+                  const SizedBox(
+                    height: 55,
+                  ),
+                   const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),
+                  ),
+              
+                  const SizedBox(
+                    height: 21,
+                  ),
+                   const Text(
+                    'Enjoy Listening To Music Enjoy Listening To MusicEnjoy Listening To MusicEnjoy Listening To MusicEnjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13,
+                      color: AppColors.grey
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+              
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicButton(onPressed: (){
+              
+                        }, 
+                        title: 'Register'
+                        ),
+                      ),
+                      const SizedBox(
+                        width:20,
+                      ),
+                      TextButton(onPressed: (){
+
+                      }, 
+                      child: Text(
+                        'Sign in'
+                      ))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
-    );
+      );
   }
 }
