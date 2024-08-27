a928239ab7f343af46e3c75c2f9cfc50da42bb56
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-27 23:12:24 +0530

    code corrected

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:music_app/core/configs/theme/app_theme.dart';
 import 'package:music_app/firebase_options.dart';
 import 'package:music_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:music_app/presentation/splash/pages/splash/splash.dart';
+import 'package:music_app/service_locator.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,6 +20,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
